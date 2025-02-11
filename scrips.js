8c272ddce623933e6b350326558aff218ad185a1
Blanca-Portillo <blancaportillo0017@gmail.com>
2025-02-11 13:08:38 -0600

    si

---
+++ scrips.js
@@ -0,0 +1,29 @@
+
+document.getElementById('hobby').addEventListener('click', function() {
+    document.getElementById('hobby-text').textContent = 'Leer libros de acción romance y comedia ';
+});
+
+
+document.getElementById('comida').addEventListener('click', function() {
+    document.getElementById('comida-text').textContent = 'Hamburguesa';
+});
+
+
+document.getElementById('Serie').addEventListener('click', function() {
+    document.getElementById('Serie-text').textContent = 'GAMES OF THRONES y HOUSE OF THE DRAGÓN';
+});
+
+document.getElementById('color').addEventListener('click', function() {
+    document.getElementById('color-text').textContent = 'Rojo';
+});
+
+
+document.getElementById('artista').addEventListener('click', function() {
+    document.getElementById('artista-text').textContent = 'Lana del Rey';
+});
+
+
+document.getElementById('pais').addEventListener('click', function() {
+        document.getElementById('pais-text').textContent = 'Colombia';
+    }
+);
